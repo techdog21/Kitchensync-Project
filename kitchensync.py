df93f76235121def46f44ee541dfb64538d5eb67
techdog21 <techdog21@mac.com>
2022-10-21 09:36:32 -0600

    Create kitchensync.py
    Start of my project.

---
+++ kitchensync.py
@@ -0,0 +1,24 @@
+# kitchensync.py
+# Author: Jerry Craft
+# Project for CSCI E-7 Python
+# October 21, 2022
+#
+# KitchenSink.py 
+# This program is an all around clean up tool I am designing to help me with my 
+# penetration testing problems.  This tool is surrounded around the idea that Tenable Nessus Pro
+# while doing a good job at vunlerability scanning, fails to provide me with reasonable reporting
+# mechanisms.  So this tool will make up for the several shortcomings by slicing, dicing, and manipulating
+# data from Tenable Nessus CSV files.
+
+# libraries
+
+# variables
+
+
+
+# main function
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
