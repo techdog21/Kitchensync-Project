f284a950435d044e742c45e8912636f9d8ba4a19
techdog21 <techdog21@mac.com>
2022-11-17 15:58:06 -0700

    commit

--- kitchensyncLive.py
+++ kitchensyncLive.py
@@ -15,13 +15,14 @@
 # merge multiple files into one file and print to output file .csv
 
 # libraries                         # anything with a *** are libraries that were outside of class.
-import os                           # read directory for files
 import sys                          # printing to a file
 import csv                          # read csv files
 import re                           # search expressions
-import subprocess                   # create subprocesses for scanning ***
 import argparse                     # commandline argument parser ***
 import requests                     # grab robots.txt and other text, html files.
+from tqdm import tqdm               # progress bar for longer processes ***
+                                    # use searchsploit library ***
+                                    # https://github.com/andreafioraldi/cve_searchsploit               
 
 # Argument Parser
 # This sets up our arguments and help/options for the user
@@ -29,17 +30,19 @@ import requests                     # grab robots.txt and other text, html files
 parser = argparse.ArgumentParser(description='Options')
 parser.add_argument("filename", type=str, help='filename of CSV to read')
 parser.add_argument('-a', '--action', action='store_true', default=False, help='Run a type of attack type [nmap / nikto / eyewitness / all], all for all attacks')
+parser.add_argument('-b', '--sBar', action='store_true', default=False, help='Generate a stacked barchart on the results of a search')
 parser.add_argument('-c', '--cAttack', action='store_true', default=False, help='Automatically additional attack files and store in all output formats.')
-parser.add_argument('-d', '--download', type=str, default='robots.txt', help='file to download ex. robots.txt.  Text file only.')
+parser.add_argument('-d', '--download', type=str, default='robots.txt', help='filename to name file after its been downloaded [ex. robots.txt].  Text file only.')
 parser.add_argument('-f', '--field', type=str, default='host', help='field to search for the searchterm = Risk')
 parser.add_argument('-g', '--cGraphics', action='store_true', default=False, help='Create a graph of the vulnerability risks.')
-parser.add_argument('-i', '--iPrint', action ='store_true', default=False, help='Print only IP addresses to a file, must be used with -p argument too')
+parser.add_argument('-ip', '--iPrint', action ='store_true', default=False, help='Print only IP addresses to a file, must be used with -p argument too')
 parser.add_argument('-m', '--cMerge', type=str, help='Identify a second file to merge with the core file, and both files data will be merged.')
 parser.add_argument('-p', '--aPrint', action='store_true', default=False, help='Print output to a file')
-parser.add_argument('-r', '--rAttack', action='store_true', default=False, help='Run the attack after the files were created')
+parser.add_argument('-q', '--query', action='store_true', default=False, help='Expand the information on a particular finding. SEARCH field cannot be a wildcard. It must be a specific search.')
 parser.add_argument('-s', '--search', type=str, default='.', help='search term to use = Critical.  [ a period . is a wildcard for all]')
 parser.add_argument('-t', '--topTen', type=int, default=False, help='Generate a top 10 list of systems, and risks')
 parser.add_argument('-w', '--webScrap', action='store_true', default=False, help='Scrap to a file. Example robots.txt')
+parser.add_argument('-x', '--eXploit', action='store_true', default=False, help='Run SearchSploit to find an exploit based upon a list of CVEs')
 args = parser.parse_args()
 
 #global variables
@@ -90,34 +93,62 @@ def printList(fields:list, lst:list) -> None:
     if (args.aPrint == True) and (args.iPrint == True):
         # print only IP addresses
         uniqueList = []
-        turnOnPrint('ip-address-output.txt') # turn on std console print
+        printfile = open('ip-address-output.txt', 'w') 
+        # add unique ip addresses to a new list
         for row in lst:
-            if row[4] not in uniqueList:
-                uniqueList.append(row[4])
+             if row[4] not in uniqueList:
+                 uniqueList.append(row[4])
+        # sort the new list
+        uniqueList.sort()
+        # print IP addresses into a file.
         for item in uniqueList:
-            uniqueList.sort()
-            print('{:<15s} '.format(item))
-
-        turnOffPrint() # my own function to turn off console print.
+            print('{:<15s} '.format(item), file= printfile)
+        printfile.close()
+        print('IP addresses printed...')
     else:
         # print standard output, and print to file if desired
         if args.aPrint == True: 
-            turnOnPrint('sink-output.txt') # turn on std console print
+            turnOnPrint('sink-output.txt') 
         # printing in columns: https://scientificallysound.org/2016/10/17/python-print3/
         for row in lst:
-            print('[+] {:<15s} {:<7s} {:<10} {:<10} {:<15} {:<20}'.format(row[4], row[6], row[5], row[3], row[1], row[7]))
+            print('[+] {:<15s} {:<7s} {:<10} {:<10} {:<15} {:.70}'.format(row[4], row[6], row[5], row[3], row[1], row[7]))
         print('---------------------------------------------------------------------------------------------------------')
         print('[=] {:<15s} {:<7s} {:<10} {:<10} {:<15} {:<20}'.format(fields[4], fields[6], fields[5], fields[3], fields[1], fields[7]))
         print("\nTotal Entries: ", len(lst)) # print record count
 
         # make a printout of the core main calcs so you can see if critical/highs exist and should be examined.
-        crit, high, med, low, non = calcRisk(lst)
+        crit, high, med, low, non = calcRisk(lst, 'all')
         print(f'Risk Criteria [Criticals: {crit}, Highs: {high}, Mediums, {med}, Lows: {low}, None: {non}]\n')
         turnOffPrint() # turn off printing
     
 ################################################################
 
-def attackFiles(lst:list) -> subprocess:
+def pQuery(lst:list) -> None:
+    #declare
+    ipLst = []
+    portLst = []
+    for rows in lst:
+        # gather IP addresses and put into a string
+        if rows[4] not in ipLst:
+            ipLst.append(rows[4])
+        if rows[6] not in portLst:
+            portLst.append(rows[6])
+    # print 1 record of what the name info, and add all the IP's affected by that issue.
+    print('-------------------------------------------------------------------')
+    print(f'[+] Name: {rows[7]}')
+    print(f'[+] IP Addresses:', ipLst)
+    print(f'[+] Ports: ', portLst)
+    print(f'[+] CVE: ', rows[1])
+    print(f'[+] Risk Level: ', rows[3])
+    print('-------------------------------------------------------------------')
+    print(f'[+] Synopsis: ', rows[8])
+    print(f'\n[+] Description: ', rows[9])
+    print('\n\n\n')
+    
+################################################################
+
+
+def attackFiles(lst:list):
     "Gather Eyewitness data / nikto data and create attack files"
     with open("eyewitness.txt", 'w') as fp:
         with open("nikto.sh", 'w') as np:
@@ -133,27 +164,27 @@ def attackFiles(lst:list) -> subprocess:
     print("files created...")
 ###################################################################
 
-def runMe(prog:str) -> bool:
-    """Take a string for the program to run, and return bool if success/fail"""
-    if prog.lower() == "eyewitness" or prog.lower() == 'all':
-        if os.path.exists('eyewitness.txt'):
-            subprocess.call(["eyewitness", "-f", "./eyewitness.txt", "--web"])
-            return True
-        else: 
-            return False
-    if prog.lower() == 'nikto' or prog.lower() == 'all':
-        if os.path.exists('nikto.sh'):
-            subprocess.call(["nikto.sh"])
-            return True
-        else:
-            return False
-    if prog.lower() == 'nmap' or prog.lower() == 'all':
-        if os.path.exists('nmap'):
-            subprocess.run(["nmap", "-sS", "-sC", '-iL', 'http-nmap.txt', "--script=http*", "-oN", 'results' + "-nmap"])
-    return False
+# def runMe(prog:str) -> bool:
+#     """Take a string for the program to run, and return bool if success/fail"""
+#     if prog.lower() == "eyewitness" or prog.lower() == 'all':
+#         if os.path.exists('eyewitness.txt'):
+#             subprocess.call(["eyewitness", "-f", "./eyewitness.txt", "--web"])
+#             return True
+#         else: 
+#             return False
+#     if prog.lower() == 'nikto' or prog.lower() == 'all':
+#         if os.path.exists('nikto.sh'):
+#             subprocess.call(["nikto.sh"])
+#             return True
+#         else:
+#             return False
+#     if prog.lower() == 'nmap' or prog.lower() == 'all':
+#         if os.path.exists('nmap'):
+#             subprocess.run(["nmap", "-sS", "-sC", '-iL', 'http-nmap.txt', "--script=http*", "-oN", 'results' + "-nmap"])
+#     return False
 #####################################################################
 
-def calcRisk(rows:list) -> bool:
+def calcRisk(lst:list, item:str) -> int:
     """ Generate risk figures for detailed data points """    
     # create counters
     ccounter = 0
@@ -161,13 +192,26 @@ def calcRisk(rows:list) -> bool:
     mcounter = 0
     lcounter = 0
     ncounter = 0
+    newLst = []
     # found this method: https://www.programiz.com/python-programming/methods/list/count
-    for row in rows:
-        ccounter += row[3].count('Critical')
-        hcounter += row[3].count('High')
-        mcounter += row[3].count('Medium')
-        lcounter += row[3].count('Low')
-        ncounter += row[3].count('None')
+    if item != 'all':
+        for rows in lst:
+            if (rows[4] == item):
+                newLst.append(rows)
+    if item != 'all':
+        for row in newLst: # calc special list
+            ccounter += row[3].count('Critical')
+            hcounter += row[3].count('High')
+            mcounter += row[3].count('Medium')
+            lcounter += row[3].count('Low')
+            ncounter += row[3].count('None')
+    else:
+        for row in lst: # calc whole list
+            ccounter += row[3].count('Critical')
+            hcounter += row[3].count('High')
+            mcounter += row[3].count('Medium')
+            lcounter += row[3].count('Low')
+            ncounter += row[3].count('None')
     # return multiple: https://note.nkmk.me/en/python-function-return-multiple-values/
     return ccounter, hcounter, mcounter, lcounter, ncounter
 #############################################################
@@ -179,7 +223,7 @@ def riskGraph(crit:int,high:int,med:int,low:int) -> bool:
     # learned alot in class, but here: https://www.geeksforgeeks.org/bar-plot-in-matplotlib/
     # count the values for each item.
     # gather keys and values
-    keys = ['Critical', 'High', 'Medium', 'Low']
+    keys = ['Critical:' + str(crit), 'High:' + str(high), 'Medium:' + str(med), 'Low:' + str(low)]
     values = [crit, high, med, low]
     answer = "Total Risks Found: " + str(crit + high + med + low)
     
@@ -195,8 +239,6 @@ def riskGraph(crit:int,high:int,med:int,low:int) -> bool:
 
     plt.show()
 ######################################################
-
-
 
 def get_pages(url) -> str:
     "Grab webpages or robots, etc."
@@ -249,25 +291,72 @@ def merge(lst:list, fil:str)-> list:
     print('Finished writing CSV file: new-merged-csv.csv.  Old file preserved.')
 ##############################################################
 
-def topTenIP(lst:list, amt:str) -> list:
-    "Get the top XX IP addresses from the lst, and then generate a list for those systems with total risk"
+def wordCloud(fields: list, lst:list, search:str):
+    #I only want to see the graphing message if we choose to graph.
+    import matplotlib.pyplot as plt  # Turn on if were graphing only.  Don't turn on globally
+    from wordcloud import WordCloud, STOPWORDS
+    stopwords = set(STOPWORDS)
+
+    comment_words = (comment_words.join(lst))
+
+    wordcloud = WordCloud(width = 800, height = 800,
+                    background_color ='white',
+                    stopwords=stopwords,
+                    min_font_size=10).generate(comment_words)
+    
+##############################################################
+
+def stakBar(lst:list):
+    "Generate a stacked bar chart to help understand results"
+    import matplotlib.pyplot as plt
+    import numpy as np
+    crit = 0
+    high = 0
+    med = 0
+    low = 0
+    nan = 0
+    newLst = []
+    c = ['#cc0000', '#ff8300', '#ffcf00', '#0000d4']
+    for row in lst:
+        if row[4] not in newLst:
+            # if IP not found in newLst
+            crit,high,med,low,nan = calcRisk(lst, row[4]) # calc risk
+            # build the bar
+            plt.bar(row[4], low, color='#0000d4')
+            plt.bar(row[4], med, bottom=low, color='#ffcf00')
+            plt.bar(row[4], high, bottom=low+med, color='#ff8300')
+            plt.bar(row[4], crit, bottom=low+med+high, color='#cc0000')
+            # append ip to new list so we don't do it again.
+            newLst.append(row[4])
+            # end when were done
+    plt.xlabel("IP Addresses")
+    plt.ylabel("Risk")
+    plt.legend(['Low', 'Medium', 'High', 'Critical'])
+    plt.title("Top Systems by Risk")
+    plt.show()
+
+###################################################################
+
+def topTenIP(lst:list, amt) -> list:
+    "Get the top 10 IP addresses from the lst, and then generate a list for those systems with total risk"
     # declare
     topIP = []
     finalLst = []
     calcLst = []
     sumRisk = 0
+    
     # get a list of host addresses to begin
     for rows in lst:
         if rows[4] not in topIP:
             topIP.append(rows[4])
     # now get a list of all vulnerabilities for these hosts
     fields, rows = openFile(args.filename)
-    for ip in topIP:
-        for row in rows:
+    for ip in tqdm(topIP, desc="Pull List:"): # run our progressbar so we can see console movement.
+        for row in rows: #https://medium.com/@harshit4084/track-your-loop-using-tqdm-7-ways-progress-bars-in-python-make-things-easier-fcbbb9233f24
             if ip == row[4]:
                 finalLst.append(row)
     # now calculate up all the risks for each host to get a top 10
-    for ip in topIP:
+    for ip in tqdm(topIP, desc='Calc List:'): # run our progressbar so we can see console movement.
         for rows in finalLst:
             if ip in rows:
                 if rows[2] != "":
@@ -276,7 +365,7 @@ def topTenIP(lst:list, amt:str) -> list:
         sumRisk = 0
     # sort all by risk value and keep top 10 in a list
     calcLst.sort(key= lambda x : x[1], reverse=True)    # return a sorted list by Risk Value
-    del(calcLst[amt:]) # got my top XX.
+    del(calcLst[amt:]) # got my top 10.
     # now get those rows that have all the detail for those IP's.
     lst.clear() # reuse lst.
     for ip in calcLst:
@@ -288,9 +377,27 @@ def topTenIP(lst:list, amt:str) -> list:
     print(f'Top systems most risky are in order: ')
     for ip in calcLst:
         print('IP: {:<16} : CVE Risk Value: {:.2f}'.format(ip[0], ip[1]))
+    
+    if args.sBar == True:
+        stakBar(lst)
+    ####################################################
 
-    #################################################
+def searchExploit(lst:list) -> None:
+    "Search for exploits using Kali version of SearchSploit"
+    import cve_searchsploit as cs # load if needed
+    resultLst = []
+    # first clone exploitdb in case its not available
+    #cs.update_db()
+    exploitFile = open('exploit.txt', 'w')
+    for rows in tqdm(lst):  # progress bar as CVE to Exploits are found
+        # find each cve as necessary
+        if cs.edbid_from_cve(rows[1]) != []:
+            if rows[1] not in resultLst: # if CVE has already been seen, move on.
+                resultLst.append(rows[1]) # add to list and print
+                print(f'CVE: {rows[1]} and Exploit: ', cs.edbid_from_cve(rows[1]), file = exploitFile)
+    exploitFile.close()
 
+#####################################################
 # main function
 def main():
     #
@@ -312,20 +419,28 @@ def main():
 
     if args.cGraphics == True:
         print('Creating graphics...')
-        a, b, c, d, e = calcRisk(lst) # I won't always use e = None
+        a, b, c, d, e = calcRisk(lst, 'all') # I won't always use e = None
         riskGraph(a,b,c,d)
     elif args.cAttack == True:
         print('Generating files.')
         attackFiles(lst)
-    elif args.rAttack == True:
-        print('Attacking systems.')
-        runMe(args.action)
-    elif args.webScrap == True:
-        print('running. file download')
-        requestPage(lst, args.download)
     elif args.cMerge != None:
         print('Merging documents')
         merge(lst, args.cMerge)
+    elif args.eXploit == True:
+        print('Searching for exploits, check exploit.txt for findings')
+        searchExploit(lst)
+
+    if (args.webScrap == True) and (args.download != ""):
+        print('running. file download')
+        requestPage(lst, args.download)
+    else:
+        print('Check your command.  In order to scrape a file you need to choose -w and -d with a filename.  [-w -d robots.txt]')
+    
+    if (args.search != '.') and (args.query == True):
+        pQuery(lst)
+    else:
+        print('Check your command.  In order to pull a query on a vulnerability name, it must be a single name item.  [-n name -s eternalblue -q')
     ###############################################
 
 # dunder start
